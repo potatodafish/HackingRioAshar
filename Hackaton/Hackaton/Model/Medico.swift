fe31e8dc51af0c9ce9fcfb85709e4a7b43c97d8c
Felipe Procaci Kestelman <fpkest@gmail.com>
2018-07-28 02:42:59 -0300

    Firebase Instalado

---
+++ Hackaton/Hackaton/Model/Medico.swift
@@ -0,0 +1,13 @@
+//
+//  Medico.swift
+//  Hackaton
+//
+//  Created by Felipe Kestelman on 28/07/2018.
+//  Copyright © 2018 Felipe Kestelman. All rights reserved.
+//
+
+import Foundation
+
+class Medico {
+    
+}
